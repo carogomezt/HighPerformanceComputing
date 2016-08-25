9e7e748a6bfde93f4a4b486c09104a75061a02ed
carogomezt <carolina9511@gmail.com>
2016-08-25 17:47:15 -0500

    change upload image with opencv

---
+++ OpenCV/Readme.md
@@ -0,0 +1,7 @@
+# How to install opencv
+
+[Step by step](http://www.linuxhispano.net/2012/11/05/instalar-opencv-2-4-2-ubuntu-12-04/)
+
+For possible install error:
+
+      sudo apt-get install qt5-default
--- OpenCV/displayImage.cpp
+++ OpenCV/displayImage.cpp
@@ -1,28 +1,30 @@
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace cv;
+using namespace std;
 
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
+    return -1;
+  }
 
-    Mat image;
-    image = imread( argv[1], 1 );
+  Mat image;
+  image = imread(argv[1], CV_LOAD_IMAGE_COLOR); // Read the file
+  cout << image.shape;
 
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+  if (!image.data) // Check for invalid input
+  {
+    cout << "Could not open or find the image" << std::endl;
+    return -1;
+  }
 
-    waitKey(0);
+  namedWindow("Display window",
+              WINDOW_AUTOSIZE);    // Create a window for display.
+  imshow("Display window", image); // Show our image inside it.
 
-    return 0;
+  waitKey(0); // Wait for a keystroke in the window
+  return 0;
 }
