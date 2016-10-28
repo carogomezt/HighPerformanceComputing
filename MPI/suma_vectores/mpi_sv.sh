e1c0b3e31bf56c5141ae48f0784f712391ece6af
Carolina Gomez T <carolina9511@gmail.com>
2016-10-28 09:35:52 -0500

    Update mpi_sv.sh

--- MPI/suma_vectores/mpi_sv.sh
+++ MPI/suma_vectores/mpi_sv.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --job-name=mpi_mm
-#SBATCH --output=res_mpi_mm.out
+#SBATCH --job-name=mpi_sv
+#SBATCH --output=res_mpi_sv.out
 #SBATCH --nodes=6
 #SBATCH --ntasks=8
 #SBATCH --time=10:00
 #SBATCH --mem-per-cpu=100
 
-mpirun mpi_mm
+mpirun mpi_sv
+
