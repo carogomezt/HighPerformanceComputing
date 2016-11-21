63050e7e49b01e67355fc9fdafc393a8b27b5217
carogomezt <carolina9511@gmail.com>
2016-11-21 12:48:51 -0500

    Changing things

--- SegundoParcial/matmult_MPI.sh
+++ SegundoParcial/matmult_MPI.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name matmult_MPI
 #SBATCH --output matmult_MPI.out
 #SBATCH --nodes 2
-#SBATCH --ntasks-per-node 8
+#SBATCH --ntasks-per-node 1
 #sbatch --gres=gpu:1
-	
+
 export CUDA_VISIBLE_DEVICES=0
 mpirun matmult
